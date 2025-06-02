c950de8ba6ced3ccd598cd1b3750fed21ef90126
Fabien Maquin <fabien@krafter.io>
2025-06-02 14:00:31 +0200

    feat(addon): add product_catalogue addon

---
+++ product_catalogue/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright Krafter SAS <hey@krafter.io>
+# Krafter Proprietary License (see LICENSE file).
+
+{
+    'name': 'Product catalogue',
+    'category': 'Technical',
+    'license': 'Other proprietary',
+    'description': 'This addon ads ability to manage product catalogue',
+    'summary': 'This addon ads ability to manage product catalogue',
+    'version': '1.0',
+    'author': 'Krafter SAS',
+    'maintainer': [
+        'Krafter SAS',
+    ],
+    'application': True,
+    'website': 'https://krafter.io',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        # Menu
+        'views/menu.xml',
+    ],
+}
